14114cde8163f8b6d79578926791c14981cf6379
Ali <info.miskeen@gmail.com>
2026-08-11 14:34:24 +0530

    file modified

--- fees.py
+++ fees.py
@@ -2,12 +2,10 @@ import tkinter as tk
 import numpy as np
 login=input("Enter your login details:")
 pwd=input("Enter your password")
-temp=3
 for i in range(3):
     pass
 print("One print statement added in github repo")
 print("Web are learning Github and git")
 print("We are from DS-B")
 print("This line is from github")
-
-
+print("We are from CSE-B Data Sciences")
