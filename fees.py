4d9195bbde92ca371683174b82963f9270fbf32b
Ali <info.miskeen@gmail.com>
2026-08-11 14:43:14 +0530

    file modofied

--- fees.py
+++ fees.py
@@ -2,10 +2,7 @@ import tkinter as tk
 import numpy as np
 login=input("Enter your login details:")
 pwd=input("Enter your password")
-<<<<<<< HEAD
-=======
-temp=30
->>>>>>> fd6fdfd20893f5ad4ca00c5f74443badc766c155
+temp=50
 for i in range(3):
     pass
 print("One print statement added in github repo")
